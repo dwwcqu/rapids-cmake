bdd8a62d2452a0854829c2a8d04bb8526814f8c5
dengww <dwwdyxiang@163.com>
2023-07-27 21:01:18 +0800

    using rocm_agent_enumerator to get the ROCm arch list

--- rapids-cmake/hip/set_architecture.cmake
+++ rapids-cmake/hip/set_architecture.cmake
@@ -3,11 +3,11 @@ if(NOT DEFINED ENV{RAPIDS_ROCM_ARCH})
 	find_program(ROCM_AGENT_EUNMERATOR rocm_agent_enumerator PATHS "/opt/rocm/bin/" REQUIRED)
 	if(ROCM_AGENT_EUNMERATOR STREQUAL "")
 		message(FATAL_ERROR "rocm_agent_enumerator cannot found")
-	endif
-	execute_process(COMMAND ${ROCM_AGENT_EUNMERATOR}
-			RESULT_VARIABLE rocm_agent_enumerator_return
-			OUTPUT_VARIABLE rocm_agent_enumerator_result)
-	if(rocm_agent_enumerator_return NOT EQUAL 0)
+	endif()
+	execute_process(COMMAND ${ROCM_AGENT_EUNMERATOR} 
+		RESULT_VARIABLE rocm_agent_enumerator_return 
+		OUTPUT_VARIABLE rocm_agent_enumerator_result)
+	if(NOT rocm_agent_enumerator_return STREQUAL 0)
 		message(FATAL_ERROR "rocm_agent_enumerator execute failed")
 	endif()
 	string(REPLACE "\n" ";" arch_gfx_list ${rocm_agent_enumerator_result})
